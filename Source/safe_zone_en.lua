f1b5d5d086ba11819ba9914fb435b441c29ececf
Senior[AmIr] <92669019+SeniorAm@users.noreply.github.com>
2024-07-14 14:23:39 +0330

    Create safe_zone_en.lua

---
+++ Source/safe_zone_en.lua
@@ -0,0 +1,22 @@
+-- safe_zone.lua
+-- Simple Safe Zone script for MTA
+
+local safeZone = { x = 1000, y = 1000, z = 10, radius = 50 }
+
+-- Function to check if a player is in the safe zone
+function isPlayerInSafeZone(player)
+    local px, py, pz = getElementPosition(player)
+    local distance = getDistanceBetweenPoints3D(px, py, pz, safeZone.x, safeZone.y, safeZone.z)
+    return distance <= safeZone.radius
+end
+
+-- Function to prevent damage to players in the safe zone
+function preventDamage(attacker, weapon, bodypart, loss)
+    if isPlayerInSafeZone(source) then
+        cancelEvent()
+        outputChatBox("🔒 You are in a Safe Zone and cannot take damage! 🔒", source, 0, 255, 0)
+    end
+end
+
+-- Attach the function to the "onPlayerDamage" event
+addEventHandler("onPlayerDamage", getRootElement(), preventDamage)
